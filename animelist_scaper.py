bf0f434c621bfc0782485099d2b66ee356a00f82
Ilmari Autio <ilmari.autio@gmail.com>
2017-05-02 10:15:24 +0300

    Store anime data in json file

--- animelist_scaper.py
+++ animelist_scaper.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import time
 import random as r
 import xml.etree.ElementTree as ET
+import json
 
 
 MAL_URL = "https://myanimelist.net/malappinfo.php?u={0}&status=all&type=anime"
@@ -13,21 +14,49 @@ INTERVAL_END = 0.5
 LOG_FILE = "events.log"
 DATA_DIR = "data"
 USERNAMES_FILE = path.join(DATA_DIR, "mal_usernames.txt")
+ANIME_LISTS_FILE = path.join(DATA_DIR, "anime_lists.json")
 
 
-def get_animelist(username):
+def get_anime_list(username):
     url = MAL_URL.format(username)
     request = urllib.request.urlopen(url)
 
     return ET.fromstring(request.read())
 
 
+def extract_data(anime_list):
+    info = anime_list.find("myinfo")
+    completed = int(info.find("user_completed").text)
+
+    if completed < 10:
+        return None
+
+    anime_data = {
+        "user_name": info.find("user_name").text,
+        "user_id": info.find("user_id").text,
+        "anime_list": []
+    }
+
+    for anime in anime_list.findall("anime"):
+        title = anime.find("series_title").text
+        score = int(anime.find("my_score").text)
+
+        if score > 0:
+            anime_data["anime_list"].append((title, score))
+
+    return anime_data
+
+
 def main():
     with open(USERNAMES_FILE, "r") as file:
         for username in file:
             try:
-                animelist = get_animelist(username.strip())
-                ET.ElementTree(animelist).write("test.xml")
+                anime_list = get_anime_list(username.strip())
+                anime_data = extract_data(anime_list)
+
+                if anime_data is not None:
+                    with open(ANIME_LISTS_FILE, "a") as anime_lists:
+                        anime_lists.write(json.dumps(anime_data) + "\n")
 
                 time.sleep(r.uniform(INTERVAL_START, INTERVAL_END))
 
