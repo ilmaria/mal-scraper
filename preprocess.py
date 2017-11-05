6e63424a68b9b063a7389024296ac31ee0567df6
Ilmari Autio <ilmari.autio@gmail.com>
2017-11-05 13:08:58 +0200

    Something

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,7 @@
 import json
 
 
-ANIME_LIST = 'data/anime_lists.json'
+ANIME_LIST = 'data/animelists.json'
 TARGET = 'Limpparipoju'
 ANIME_NAMES = 'anime_names.txt'
 RATINGS = 'anime_ratings.txt'
@@ -19,33 +19,39 @@ def main():
         pass
 
     with open(ANIME_LIST, 'r') as file:
-        for line in file:
-            entry = json.loads(line)
+        for i, line in enumerate(file):
+            try:
+                entry = json.loads(line)
 
-            for anime in entry['anime_list']:
-                anime_ids[int(anime['anime_id'])] = 0
+                for anime in entry['anime_list']:
+                    anime_ids[int(anime['anime_id'])] = 0
 
-                with open(ANIME_NAMES, 'a') as id_file:
-                    id_file.write(anime['anime_id'] + '\t' + anime['title'] + '\n')
+                    with open(ANIME_NAMES, 'a') as id_file:
+                        id_file.write(anime['anime_id'] + '\t' + anime['title'] + '\n')
+            except:
+                print("Processing anime_ids. Current line: " + str(i) + "\n\n")
 
     with open(ANIME_LIST, 'r') as file:
-        for line in file:
-            entry = json.loads(line)
-            anime_ids = anime_ids.fromkeys(anime_ids, 0)
+        for i, line in enumerate(file):
+            try:
+                entry = json.loads(line)
+                anime_ids = anime_ids.fromkeys(anime_ids, 0)
 
-            for anime in entry['anime_list']:
-                anime_ids[int(anime['anime_id'])] = int(anime['score'])
+                for anime in entry['anime_list']:
+                    anime_ids[int(anime['anime_id'])] = int(anime['score'])
 
-            if entry['user_name'] == TARGET:
-                ratings_file = TARGET + '_ratings.txt'
-            else:
-                ratings_file = RATINGS
+                if entry['user_name'] == TARGET:
+                    ratings_file = TARGET + '_ratings.txt'
+                else:
+                    ratings_file = RATINGS
 
-                with open(USERNAMES, 'a') as usernames:
-                    usernames.write(entry['user_name'] + '\n')
+                    with open(USERNAMES, 'a') as usernames:
+                        usernames.write(entry['user_name'] + '\n')
 
-            with open(ratings_file, 'a') as ratings:
-                ratings.write(' '.join(map(lambda x: str(x), anime_ids.values())) + '\n')
+                with open(ratings_file, 'a') as ratings:
+                    ratings.write(' '.join(map(lambda x: str(x), anime_ids.values())) + '\n')
+            except:
+                print("Processing ratings. Current line: " + str(i) + "\n\n")
                 
 
 
